e3281921be400ea6aafaa26d3aae460d6e88d5f7
Murty Gudipati <murty.gudipati@saven.in>
2015-03-05 14:03:24 +0530

    config file using javascript

---
+++ config.js
@@ -0,0 +1,59 @@
+var config = {}
+
+config.ctf = {}
+config.ctf.host="198.190.11.21"
+config.ctf.port=4012
+config.ctf.commands = [
+  "5022=LoginUser|5028=tiger|5029=tiger|5026=1",
+  //"5022=LoginUser|5028=pfcanned|5029=cypress|5026=1",
+  //"5022=LoginUser|5028=plusserver|5029=plusserver|5026=1",
+  //"5022=ListAdministrationInfo|5026=2",
+  //"5022=ListSystemPermission|5026=3",
+  //"5022=ListUserPermission|5026=4",
+  "5022=SelectAvailableTokens|5026=5",
+  //"5022=ListAvailableTokens|5026=6",
+  //"5022=ListEnumeration|5026=7",
+  //"5022=ListExchangeTokens|4=941|5026=8",
+  //"5022=SelectUserTokens|5035=5|5035=308|5035=378|5026=9",
+  //"5022=ListUserTokens|5026=10",
+  //"5022=QuerySnap|4=941|5=E:TCS.EQ|5026=11",
+  //"5022=QuerySnap|4=1057|5=IBM|5026=11",
+  //"5022=QuerySnap|4=941|5026=11",
+  //"5022=QuerySnap|4=938|5=E:941|5026=11",
+  //"5022=Subscribe|4=13542|5026=12",
+  //"5022=Unsubscribe|5026=13",
+  //"5022=SelectOutput|5018=ON|5026=14",
+  //"5022=ListSplitExchanges|5026=15",
+  //"5022=QueryWildCard|4=941|3177={machine}|5026=16",
+  //"5022=QuerySubscribedExchanges|5026=17",
+  //"5022=QuerySubscribedSymbols|5026=18",
+  //"5022=ListSubscribedExchanges|5026=19",
+  //"5022=ListSubscribedNews|5026=20",
+  //"5022=ListSubscribedSymbols|5026=21",
+  //"5022=QuerySnapAndSubscribe|4=1057|5026=22",
+  //"5022=QuerySnapAndSubscribe|4=941|5=E:TCS.EQ|5026=22",
+  //"5022=QueryDepth|4=328|5=IBM|5026=24",
+  //"5022=QuerySnapAndSubscribe|4=938|5=E:941|5026=11",
+  "5022=QueryDepth|4=938|5=E:941|5026=11",
+  //"5022=QueryDepth|4=938|5026=11",
+  //"5022=QueryDepthAndSubscribe|4=328|5=IBM|5026=23",
+  //"5022=QueryTasDates|5026=24",
+  //"5022=QueryTas|4=1057|5=IBM|5026=25"
+  //"5022=QueryTas|5040=CORRECTED|4=558|5=IBM|5026=6|5045=100",
+  //"5022=QueryDepthAndSubscribe|4=249|5=IBM|5026=7",
+  //"5022=QueryCorrections|5042=1223546400|5049=1224151200|4=558|5=IBM|5026=10",
+  //"5022=QueryHistory|49227=1223546400|49228=1224151200|4=558|5=IBM|5026=11",
+  //"5022=QueryHistory|49227=1214904600|49228=1225272600|4=558|5=IBM|5026=12",
+  //"5022=QueryInterval|5043=10|5042=1223546400|5049=1224151200|4=558|5=IBM|5026=13",
+  //"5022=QueryTas|7=P|5042=1223546400|5049=1224151200|49168=Q|5040=CORRECTED|4=558|5=IBM|5026=14",
+  //"5022=QueryTas|7=P|5042=1223546400|5049=1224151200|5040=CORRECTED|4=558|5=IBM|5026=15",
+  //"5022=QueryTas|5042=1224669600|5049=1224680400|49168=B|5040=CORRECTED|4=558|5=IBM|5026=16",
+  //"5022=QueryTas|5042=1224669600|5049=1224680400|49168=T|5040=corrected|4=558|5=IBM|5026=17",
+  //"5022=QueryTas|5042=1224669600|5049=1224680400|49168=T|5040=uncorrected|4=558|5=IBM|5026=18",
+  //"5022=QueryTas|5042=1224669600|5049=1224680400|5040=CORRECTED|4=558|5=IBM|5026=19",
+  //"5022=QueryTas|5042=1224669600|5049=1224680400|5040=CORRECTED|4=558|5=IBM|5026=20",
+  //"5022=QueryTas|7=|5042=1225310400|5049=1225317600|5040=CORRECTED|4=558|5=IBM|5026=21",
+  //"5022=QueryTas|7=|5042=1225704600|5049=1225715400|49168=B|5040=CORRECTED|4=558|5=IBM|5026=22"
+]
+
+module.exports = config;
--- ctf-client.js
+++ ctf-client.js
@@ -2,15 +2,15 @@ var net = require('net'),
     events = require('events'),
     util = require('util'),
     winston = require('winston')
-    iniparser = require('iniparser'),
     ctf = require('./ctf'),
+    config = require('./config')
 
     // global ctf socket stream
     _ctfStream = null,
     
     // global ctf client
     _ctfClient = null;
-    
+  
 //
 // Create a new winston logger instance with two tranports: Console, and File
 //
@@ -31,59 +31,13 @@ var _logger = new (winston.Logger)({
   ]
 });
   
-// parse ini configuration file
-var _ini = iniparser.parseSync('./config.ini');
-_logger.debug(JSON.stringify(_ini));
-
-//
-// global settings
-//
-var _prop = {};
-
-// ctf configuration
-_prop.ctf = {};
-_prop.ctf.host = _ini.CTF.Host;
-_prop.ctf.port = _ini.CTF.Port;
-_prop.ctf.userid = _ini.CTF.UserID;
-_prop.ctf.password = _ini.CTF.Password;
-_prop.ctf.exch = _ini.CTF.Exchanges.split(",");
-_prop.fieldmap = _ini.FieldMap;
-
-//    
-// master securities = { 
-//  securities: [
-//    {SourceID: 938, TickerSymbol: E:941},
-//    {SourceID: 938, TickerSymbol: E:942},
-//    {SourceID: 938, TickerSymbol: E:943},
-//    ...
-//    ...
-//    {SourceID: 138, TickerSymbol: IBM},
-//  ]
-// };
-
-var arr = new Array();
-_prop.ctf.exch.forEach(function(exch, pos) {
-  var section = _ini[exch];
-  if (section) {
-    if (section.SourceID && section.Tickers) {
-      section.Tickers.split(",").forEach(function(ticker, pos) {
-        arr.push({SourceID: section.SourceID,
-                  TickerSymbol: ticker
-                });
-      });
-    }
-  }
-});
-_prop.securities = arr;
-_logger.debug(JSON.stringify(_prop));
-
 // Initialize CTF Connection
 initCTF();
 
 /*
  */
 function initCTF () {
-  _ctfStream = net.createConnection(_prop.ctf.port, _prop.ctf.host, function() {
+  _ctfStream = net.createConnection(config.ctf.port, config.ctf.host, function() {
     _logger.info("established ctf connection...");
 
     // CTF Client Object
@@ -99,15 +53,10 @@ function initCTF () {
       }
     });
 
-    // send login command
-    _ctfClient.sendCommand("5022=LoginUser|5028="+_prop.ctf.userid+"|5029="+_prop.ctf.password+"|5026=1");
-    _ctfClient.sendCommand("5022=SelectAvailableTokens|5026=2");
-
-    // send QueryDepth commands
-  	_prop.securities.forEach(function(sec, pos) {
-  	  var cmd = "5022=QueryDepth|4="+sec.SourceID+"|5="+sec.TickerSymbol+"|5026="+10+pos;
+    // send CTF commands
+    config.ctf.commands.forEach(function(cmd) {
       _ctfClient.sendCommand(cmd);
-  	});
+    });
   });
 
   _ctfStream.addListener("end", function () {
