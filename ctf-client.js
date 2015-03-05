c9a131a9e046f4ee5560357a8a1a82eae35ddad6
Murty Gudipati <murty.gudipati@saven.in>
2015-03-05 08:54:43 +0530

    added ini parser

--- ctf-client.js
+++ ctf-client.js
@@ -2,6 +2,7 @@ var net = require('net'),
     events = require('events'),
     util = require('util'),
     winston = require('winston')
+    iniparser = require('iniparser'),
     ctf = require('./ctf'),
 
     // global ctf socket stream
@@ -29,14 +30,60 @@ var _logger = new (winston.Logger)({
     })
   ]
 });
-    
+  
+// parse ini configuration file
+var _ini = iniparser.parseSync('./config.ini');
+_logger.debug(JSON.stringify(_ini));
+
+//
+// global settings
+//
+var _prop = {};
+
+// ctf configuration
+_prop.ctf = {};
+_prop.ctf.host = _ini.CTF.Host;
+_prop.ctf.port = _ini.CTF.Port;
+_prop.ctf.userid = _ini.CTF.UserID;
+_prop.ctf.password = _ini.CTF.Password;
+_prop.ctf.exch = _ini.CTF.Exchanges.split(",");
+_prop.fieldmap = _ini.FieldMap;
+
+//    
+// master securities = { 
+//  securities: [
+//    {SourceID: 938, TickerSymbol: E:941},
+//    {SourceID: 938, TickerSymbol: E:942},
+//    {SourceID: 938, TickerSymbol: E:943},
+//    ...
+//    ...
+//    {SourceID: 138, TickerSymbol: IBM},
+//  ]
+// };
+
+var arr = new Array();
+_prop.ctf.exch.forEach(function(exch, pos) {
+  var section = _ini[exch];
+  if (section) {
+    if (section.SourceID && section.Tickers) {
+      section.Tickers.split(",").forEach(function(ticker, pos) {
+        arr.push({SourceID: section.SourceID,
+                  TickerSymbol: ticker
+                });
+      });
+    }
+  }
+});
+_prop.securities = arr;
+_logger.debug(JSON.stringify(_prop));
+
 // Initialize CTF Connection
 initCTF();
 
 /*
  */
 function initCTF () {
-  _ctfStream = net.createConnection(4012, "198.190.11.21", function() {
+  _ctfStream = net.createConnection(_prop.ctf.port, _prop.ctf.host, function() {
     _logger.info("established ctf connection...");
 
     // CTF Client Object
@@ -53,9 +100,14 @@ function initCTF () {
     });
 
     // send login command
-    _ctfClient.sendCommand("5022=LoginUser|5028=tiger|5029=tiger|5026=1");
+    _ctfClient.sendCommand("5022=LoginUser|5028="+_prop.ctf.userid+"|5029="+_prop.ctf.password+"|5026=1");
     _ctfClient.sendCommand("5022=SelectAvailableTokens|5026=2");
-    _ctfClient.sendCommand("5022=QueryDepth|4=938|5=E:941|5026=3");
+
+    // send QueryDepth commands
+  	_prop.securities.forEach(function(sec, pos) {
+  	  var cmd = "5022=QueryDepth|4="+sec.SourceID+"|5="+sec.TickerSymbol+"|5026="+10+pos;
+      _ctfClient.sendCommand(cmd);
+  	});
   });
 
   _ctfStream.addListener("end", function () {
